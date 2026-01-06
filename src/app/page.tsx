6086858a2df025a83240b5377e52053359ede4ad
Freddy <freddyhmk@gmail.com>
2026-01-06 17:20:54 +0800

    ugh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
-import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+// import { auth } from "~/server/auth";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   // const hello = await api.post.hello({ text: "from tRPC" });
