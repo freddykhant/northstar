38b2d8fa7f658a3a184846ea55230feec411078b
Freddy <freddyhmk@gmail.com>
2026-01-13 22:32:22 +0800

    .

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,20 +1,10 @@
 // src/db.ts
-import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
-import * as schema from "./schema";
+import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "~/env";
 
-// Only load .env in development
-if (process.env.NODE_ENV !== "production") {
-  config({ path: ".env" }); // or .env.local
-}
+config({ path: ".env" }); // or .env.local
 
-// Use env validation for DATABASE_URL
-const sql = neon(env.DATABASE_URL, {
-  // Increase timeout for production
-  fetchConnectionCache: true,
-});
-
-export const db = drizzle({ client: sql, schema });
-export { schema };
+const sql = neon(env.STORAGE_DATABASE_URL!);
+export const db = drizzle({ client: sql });
